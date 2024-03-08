f9c7260dccc485fdd30f847436599a6c4fd1695c
najimali <mohd.najim@zyod.com>
2024-03-08 16:10:20 +0530

    Added Firebase hosting

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEZuzzwzit-0anY4z8johd-bWwvA2ru44",
+  authDomain: "jira-board-4994a.firebaseapp.com",
+  projectId: "jira-board-4994a",
+  storageBucket: "jira-board-4994a.appspot.com",
+  messagingSenderId: "712388449547",
+  appId: "1:712388449547:web:984a98d50ad809a6a6be0e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
